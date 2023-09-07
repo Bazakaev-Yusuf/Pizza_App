c8ed853b40fb279228d1c57cc5fd15f7414bac6c
Yusuf <yusupkiller71@mail.ru>
2023-09-07 15:43:17 +0300

    lazy imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,72 @@
-import { FC } from 'react';
+import { FC, Suspense, lazy } from 'react';
 
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-import FullSizePizza from './pages/FullSizePizza';
-import MainLayout from './layout/MainLayout';
+// import NotFound from './pages/NotFound';
+// import Cart from './pages/Cart';
+// import FullSizePizza from './pages/FullSizePizza';
+// import MainLayout from './layout/MainLayout';
 
 import { Route, Routes } from 'react-router-dom';
 
+import fallbackImg from './assets/img/not_found_pizza.png';
+
 import './scss/app.scss';
+
+const Cart = lazy(() => import(/* webpackChunkName: 'Cart' */ './pages/Cart'));
+const FullSizePizza = lazy(
+	() =>
+		import(/* webpackChunkName: 'FullSizePizza' */ './pages/FullSizePizza'),
+);
+const MainLayout = lazy(
+	() => import(/* webpackChunkName: 'MainLayout' */ './layout/MainLayout'),
+);
+const NotFound = lazy(
+	() => import(/* webpackChunkName: 'NotFound' */ './pages/NotFound'),
+);
 
 const App: FC = () => {
 	return (
 		<Routes>
 			<Route
 				path='/'
-				element={<MainLayout />}>
+				element={
+					<Suspense>
+						<MainLayout />
+					</Suspense>
+				}>
 				<Route
 					path=''
 					element={<Home />}
 				/>
 				<Route
 					path='cart'
-					element={<Cart />}
+					element={
+						<Suspense
+							fallback={
+								<img
+									src={fallbackImg}
+									className='fallback-img'
+								/>
+							}>
+							<Cart />
+						</Suspense>
+					}
 				/>
 				<Route
 					path='pizza/:id'
-					element={<FullSizePizza />}
+					element={
+						<Suspense>
+							<FullSizePizza />
+						</Suspense>
+					}
 				/>
 				<Route
 					path='*'
-					element={<NotFound />}
+					element={
+						<Suspense>
+							<NotFound />
+						</Suspense>
+					}
 				/>
 			</Route>
 		</Routes>
